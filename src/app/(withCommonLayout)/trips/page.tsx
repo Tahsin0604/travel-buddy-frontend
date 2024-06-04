fb0c0f1d995980e7f2c96e2965278d56256065d3
Natik Alam Tahsin <natikalamtahsin@gmail.com>
2024-06-04 15:58:23 +0600

    trips updated

--- src/app/(withCommonLayout)/trips/page.tsx
+++ src/app/(withCommonLayout)/trips/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useGetAllTripsQuery } from "@/redux/api/tripsApi";
-import { useEffect, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import SearchField from "./components/SearchField";
 import { TripTypeConstant } from "@/constants/trips";
 import MultipleChoice from "./components/MultipleChoice";
@@ -8,7 +8,8 @@ import DateChoice from "./components/DateChoice";
 import { useDebounced } from "@/redux/hook";
 import dayjs from "dayjs";
 import TripCard from "@/components/UI/TripCard/TripCard";
-import { Divider } from "antd";
+import { Button, Divider, Skeleton } from "antd";
+import TripsDrawer from "./components/TripsDrawer";
 
 const TripsPage = ({
   searchParams,
@@ -17,6 +18,7 @@ const TripsPage = ({
 }) => {
   const dateFormat = "YYYY-MM-DD";
   const date = dayjs(new Date()).format(dateFormat);
+  const [open, setOpen] = useState(false);
   const [startDate, setStartDate] = useState(searchParams?.startDate || date);
   const [endDate, setEndDate] = useState(searchParams?.endDate || "");
   const [tripType, setTripType] = useState(searchParams?.tripType || "");
@@ -56,17 +58,53 @@ const TripsPage = ({
 
   const { data, isLoading } = useGetAllTripsQuery({ ...searchQuery });
   const trips = data?.trips as Record<string, any>[];
-  const meta = data?.meta;
-
+  const meta = data?.meta as Record<string, any>;
+  let renderComponent: ReactNode;
+  if (isLoading) {
+    const renderComponent = (
+      <>
+        {Array.from({ length: 8 }, (v, i) => i).map((item) => (
+          <Skeleton key={item} active className="col-span-3" />
+        ))}
+      </>
+    );
+  } else {
+    renderComponent = (
+      <>
+        {trips.map((trip: Record<string, any>) => (
+          <div
+            key={trip?.id}
+            className="col-span-12 md:col-span-6 lg:col-span-4 "
+          >
+            <TripCard trip={trip} />
+          </div>
+        ))}
+      </>
+    );
+  }
   return (
     <div className="custom-container">
+      <TripsDrawer
+        open={open}
+        setOpen={setOpen}
+        date={date}
+        destination={destination as string}
+        endDate={endDate as string}
+        startDate={startDate as string}
+        tripType={tripType as string}
+        setDestination={setDestination}
+        setStartDate={setStartDate}
+        setEndDate={setEndDate}
+        setTripType={setTripType}
+        setPage={setPage}
+      />
       <p className="text-6xl font-extrabold mb-4 text-center">Find Trips</p>
       <p className="text-xl font-bold  text-center mb-20">
         Discover your next big adventure
       </p>
       <Divider />
       <div className="flex gap-4  py-20">
-        <div className="w-64 space-y-5 hidden lg:block">
+        <div className="w-1/4 space-y-5 hidden lg:block">
           <SearchField
             label="Destination"
             value={destination as string}
@@ -89,16 +127,23 @@ const TripsPage = ({
             setDate={setEndDate}
           />
         </div>
-
-        <div className="w-full grid grid-cols-12 gap-4">
-          {trips.map((trip: Record<string, any>) => (
-            <div
-              key={trip?.id}
-              className="col-span-12 md:col-span-6 lg:col-span-3 "
+        <div>
+          <div className="flex justify-between items-centers">
+            {trips && <p className="text-slate-700 font-bold text-lg">
+              {meta?.total} trips found
+            </p>}
+            <div className="lg:hidden">
+            <Button
+              
+              onClick={() => {
+                setOpen(true);
+              }}
             >
-              <TripCard trip={trip} />
+              Filter
+            </Button>
             </div>
-          ))}
+          </div>
+          <div className="w-full lg:w-3/4 grid grid-cols-12 gap-4"></div>
         </div>
       </div>
     </div>
