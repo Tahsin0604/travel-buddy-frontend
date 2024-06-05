d3681ec5bc595c0f3c471c193db10ac2241a0fb8
Natik Alam Tahsin <natikalamtahsin@gmail.com>
2024-06-05 13:17:32 +0600

    register user fixed

--- src/services/actions/registerUser.ts
+++ src/services/actions/registerUser.ts
@@ -1,7 +1,9 @@
-"use server";
 const registerUser = async (data: any) => {
   const res = await fetch(`http://localhost:5000/api/register/user`, {
     method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
     body: JSON.stringify(data),
     cache: "no-cache",
   });
