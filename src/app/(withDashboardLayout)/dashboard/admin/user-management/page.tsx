76b603afac4ddb3fb7d0852103a3d41cbcf6f9c8
Natik Alam Tahsin <natikalamtahsin@gmail.com>
2024-06-07 23:53:11 +0600

    user management

--- src/app/(withDashboardLayout)/dashboard/admin/user-management/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/user-management/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import PaginationComponent from "@/components/Reusable/PaginationComponent/PaginationComponent";
 import { useGetAllUsersQuery } from "@/redux/api/adminApi";
 import { UserOutlined } from "@ant-design/icons";
