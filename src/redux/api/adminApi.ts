6dc45f48cc566887c67b099b74e52e2be62bac61
Natik Alam Tahsin <natikalamtahsin@gmail.com>
2024-06-07 23:54:55 +0600

    user management

--- src/redux/api/adminApi.ts
+++ src/redux/api/adminApi.ts
@@ -30,7 +30,7 @@ export const adminApi = baseApi.injectEndpoints({
       },
       transformResponse: (data, meta) => {
         return {
-          trips: data,
+          users: data,
           meta: meta,
         };
       },
