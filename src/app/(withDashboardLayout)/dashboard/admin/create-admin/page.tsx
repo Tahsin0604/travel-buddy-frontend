715395fafdd996651c947434d5f6f25736d58737
Natik Alam Tahsin <natikalamtahsin@gmail.com>
2024-06-07 21:38:10 +0600

    admin

--- src/app/(withDashboardLayout)/dashboard/admin/create-admin/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/create-admin/page.tsx
@@ -11,24 +11,14 @@ import { z } from "zod";
 import { toast } from "sonner";
 import registerAdmin from "@/services/actions/registerAdmin";
 
-const createAdmin = z
-  .object({
-    name: z.string(),
-    email: z.string().email(),
-    password: z
-      .string()
-      .min(6, { message: "Must be 6 or more characters long" }),
-    confirmPassword: z
-      .string()
-      .min(6, { message: "Must be 6 or more characters long" }),
-    profile: z.object({
-      dateOfBirth: z.string(),
-    }),
-  })
-  .refine((data) => data.password === data.confirmPassword, {
-    message: "Passwords don't match",
-    path: ["confirmPassword"],
-  });
+const createAdmin = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string().min(6, { message: "Must be 6 or more characters long" }),
+  profile: z.object({
+    dateOfBirth: z.string(),
+  }),
+});
 
 const CreateAdminPage = () => {
   const [error, setError] = useState("");
@@ -48,9 +38,9 @@ const CreateAdminPage = () => {
     }
   };
   return (
-    <div className="bg-slate-50 py-10 min-h-screen">
-      <div className="grid grid-cols-12 justify-center items-center bg-slate-50 gap-8 lg:gap-16 px-8 md:px-16 lg:px-28">
-        <div className="col-span-12 md:col-span-6 px-10 py-6 rounded-lg bg-white drop-shadow-md">
+    <div className=" min-h-screen">
+      <div className="flex justify-center   px-8 md:px-16 lg:px-28">
+        <div className="w-full md:w-3/4 lg:w-1/2 px-10 py-6 rounded-lg bg-white drop-shadow-md">
           <h1 className="mb-6 text-3xl text-center font-bold text-slate-800">
             Create new Admin !!
           </h1>
