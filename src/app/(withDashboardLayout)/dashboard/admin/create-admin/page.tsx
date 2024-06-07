ac22890f735d1f6073e48cd1877d69f161034cbf
Natik Alam Tahsin <natikalamtahsin@gmail.com>
2024-06-07 21:39:06 +0600

    admin

--- src/app/(withDashboardLayout)/dashboard/admin/create-admin/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/create-admin/page.tsx
@@ -38,32 +38,30 @@ const CreateAdminPage = () => {
     }
   };
   return (
-    <div className=" min-h-screen">
-      <div className="flex justify-center   px-8 md:px-16 lg:px-28">
-        <div className="w-full md:w-3/4 lg:w-1/2 px-10 py-6 rounded-lg bg-white drop-shadow-md">
-          <h1 className="mb-6 text-3xl text-center font-bold text-slate-800">
-            Create new Admin !!
-          </h1>
-          {error && (
-            <div>
-              <p className="bg-red-500 py-2 text-white text-center rounded mt-1">
-                {error}
-              </p>
-            </div>
-          )}
-          <ReusableForm
-            onSubmit={handleSubmit}
-            resolver={zodResolver(createAdmin)}
-          >
-            <InputField name="name" label="Name" />
-            <InputField name="email" label="Email" />
-            <PasswordField name="password" label="Password" />
-            <DatePickerField name="profile.dateOfBirth" label="Date of Birth" />
-            <Button size="large" className="w-full" htmlType="submit">
-              Register
-            </Button>
-          </ReusableForm>
-        </div>
+    <div className="flex justify-center   px-8 md:px-16 lg:px-28">
+      <div className="w-full md:w-3/4 lg:w-1/2 px-10 py-6 rounded-lg bg-white drop-shadow-md">
+        <h1 className="mb-6 text-3xl text-center font-bold text-slate-800">
+          Create new Admin !!
+        </h1>
+        {error && (
+          <div>
+            <p className="bg-red-500 py-2 text-white text-center rounded mt-1">
+              {error}
+            </p>
+          </div>
+        )}
+        <ReusableForm
+          onSubmit={handleSubmit}
+          resolver={zodResolver(createAdmin)}
+        >
+          <InputField name="name" label="Name" />
+          <InputField name="email" label="Email" />
+          <PasswordField name="password" label="Password" />
+          <DatePickerField name="profile.dateOfBirth" label="Date of Birth" />
+          <Button size="large" className="w-full" htmlType="submit">
+            Register
+          </Button>
+        </ReusableForm>
       </div>
     </div>
   );
