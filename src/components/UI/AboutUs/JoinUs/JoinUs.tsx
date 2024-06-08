6eda4b6ac3fd52a13b6962c0711a43f95b6a2e23
Natik Alam Tahsin <natikalamtahsin@gmail.com>
2024-06-08 18:07:47 +0600

    logic error fixed

--- src/components/UI/AboutUs/JoinUs/JoinUs.tsx
+++ src/components/UI/AboutUs/JoinUs/JoinUs.tsx
@@ -15,22 +15,22 @@ const JoinUs = () => {
       />
       <div className="absolute inset-0 flex flex-col justify-center items-center bg-black bg-opacity-70 text-center px-10">
         <div className="text-center">
-          <h1 className="text-4xl font-semibold mb-1 text-white">
+          <h1 className="text-2xl lg:text-4xl font-semibold mb-1 text-white">
             Share Your Journey and Unique Experiences.
           </h1>
-          <h1 className="text-4xl font-semibold mb-8 text-white">
+          <h1 className="text-2xl lg:text-4xl font-semibold mb-8 text-white">
             Become Part of Our Community.
           </h1>
           <div className="flex justify-center items-center gap-6">
             <Link
               href="/dashboard/user/create-trips"
-              className="text-xl  bg-[#4096FF] px-7 py-2  rounded-3xl mt-6 text-white hover:scale-105 transition-transform duration-150"
+              className="text-base md:text-xl  bg-[#4096FF] px-4 md:px-7 py-2  rounded-3xl mt-6 text-white hover:scale-105 transition-transform duration-150"
             >
               CREATE A TRIP
             </Link>
             <Link
               href="/trips"
-              className="text-xl px-7 py-2 rounded-3xl mt-6 text-white bg-red-400 hover:scale-105 transition-transform duration-150"
+              className="text-base md:text-xl px-4 md:px-7 py-2 rounded-3xl mt-6 text-white bg-red-400 hover:scale-105 transition-transform duration-150"
             >
               JOIN A TRIP
             </Link>
